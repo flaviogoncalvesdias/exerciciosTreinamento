8c342385a22ecc12bf93b1a192eb60ad6b5f1115
Marcio Guedes Souza <mguedess@everis.com>
2019-04-30 12:18:34 -0300

    Execicio 2 concluído

---
+++ AluraJavaExercicios/src/Exercicios/Exercicio2.java
@@ -0,0 +1,18 @@
+package Exercicios;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		int num;
+		
+		for(num = 0; num <=10; num++) { 
+			if(num % 2 == 0) {
+				System.out.println( num + " Este número é par!");
+			}else {
+				System.out.println( num + " Este número é impar!");
+			}
+		}
+
+	}
+
+}
