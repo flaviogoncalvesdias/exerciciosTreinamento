3c8549ab24c7520c9dba4c64a6aac1033e591bc4
Marcio Guedes Souza <mguedess@everis.com>
2019-05-01 15:28:35 -0300

    Exercicio Random

---
+++ AluraJavaExercicios/src/Exercicios/ExercicioRandom.java
@@ -0,0 +1,14 @@
+package Exercicios;
+import java.util.Random;// importando a classe Random para que possa ultilizar os metodos dela.
+
+
+public class ExercicioRandom {
+
+	public static void main(String[] args) {
+		Random aleatorio = new Random (); //Instanciando um objeto do tipo random.
+
+		int num = aleatorio.nextInt(1000); //Aplicando o tamanho dentro do metodo nextInt que é 1000.
+		System.out.println("O número sorteado é: " + num);
+	}
+
+}
